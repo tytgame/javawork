e4b43d92ed0a5b27d78c39ccb3272e21a91dc59f
yuyeseong <yuyeseong@naver.com>
2024-12-23 16:03:27 +0900

    12.23 3교시

---
+++ chap01_print/src/chap01_print/Print01.java
@@ -0,0 +1,35 @@
+package chap01_print;
+
+public class Print01 {
+
+	public static void main(String[] args) {
+		//java파일 법칙
+		/*
+		 * 첫 글자는 대문자로 시작 - 권장사항
+		 *  반드시 첫 글자는 숫자로 시작할 수 없음
+		 *   ChapPrint Chap_print
+		 */
+		// 한줄 주석 : 컴파일이 되지 않는다 (실행이 되지 않는다)
+		// 한줄 주석의 단축키 ctrl + /
+		/* 여러줄 주석
+		 * 여기부터
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 *  여기까지 주석 
+		 *  단축키 : 블럭을 잡고 ctrl + shift + /
+		 */
+		
+		// 화면에 출력해주는 프로그램
+		// 첫 글자 S 대문자, 문자열일때 쌍 따옴표 , 끝에 세미콜론
+		// ln : 라인, 줄 바꿈
+		System.out.println("본인이름 : 유예성");
+		System.out.println("더조은 아카데미 학원");
+		System.out.println("강남에서 제일 좋은 학원");
+		
+
+	}
+
+}
---
+++ chap01_print/src/chap01_print/Println_Print_02.java
@@ -0,0 +1,13 @@
+package chap01_print;
+
+public class Println_Print_02 {
+
+	public static void main(String[] args) {
+		// print : 출력한 후 줄바꿈하지 않음
+		// println : 출력한 후 줄바꿈 함
+		System.out.println("Hello world!");
+		System.out.print("안녕~~");
+		System.out.print("유예성이라고 해");
+	}
+
+}
